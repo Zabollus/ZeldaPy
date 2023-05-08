a8f046bcd14d6dc1f100a71524124561f288cb6f
Tomasz Jakubiak <tomek.jakubiak94@gmail.com>
2023-05-09 01:28:38 +0200

    drawing of player

--- code/level.py
+++ code/level.py
@@ -3,12 +3,13 @@ from settings import *
 from tile import Tile
 from player import Player
 
+
 class Level:
     def __init__(self):
         self.display_surface = pygame.display.get_surface()
 
         self.visible_sprites = pygame.sprite.Group()
-        self.obstacles_sprites = pygame.sprite.Group()
+        self.obstacle_sprites = pygame.sprite.Group()
 
         self.create_map()
 
@@ -18,7 +19,9 @@ class Level:
                 x = col_index * TILESIZE
                 y = row_index * TILESIZE
                 if col == 'x':
-                    Tile((x, y), [self.visible_sprites])
+                    Tile((x, y), [self.visible_sprites, self.obstacle_sprites])
+                if col == 'p':
+                    Player((x, y), [self.visible_sprites])
 
     def run(self):
         self.visible_sprites.draw(self.display_surface)
